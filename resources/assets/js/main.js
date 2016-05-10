cb324f794b5c34385d13049c76e7673e7d732dd3
Thomas Bricquet <thomas.bricquet@gmail.com>
2016-05-10 16:35:10 +0200

    reinitialisation du projet avec react-codemirror

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,83 +1,160 @@
 ' use strict'
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Codemirror = require('react-codemirror');
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/markdown/markdown');
 
 
-// console.log(myCodeMirror);
 
-// function setOutput(val){
-// 	var result = document.getElementById('code');
-// 	console.log(result.innerHTML);
-// 	$(".result").append(result.innerHTML);
-// }
-// setOutput();
 
-var Message = React.createClass({
-	render : function() {
-		return (
-			<div>{this.props.text}</div>
-			);
-	}
-});
 
-var Input = React.createClass({
-	inputChange: function(e){
-		this.props.onChange(e.target.value)
-	},
-	handleSubmit : function(e) {
-		e.preventDefault();
-		this.setState({text: ' '});
-	},
-	render : function() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-			<label for="">Type your Markdown</label>
-			<textarea onChange={this.inputChange} value={this.props.text} id="code" cols="150" rows="20"/>
-			</form>
-			);
-	}
-});
-var Result = React.createClass({
+var App = React.createClass({
+ 	componentDidMount: function(){
+ 		console.log("didmount")
+ 		// var result = this.options.getValue(); 
+
+ 		 // function  prout(){
+ 		 // 	console.log(result);
+ 		 // }
+		 // setInterval(prout, 1050)
+ 	},
 	getInitialState: function(){
-		return {text:''};
+		return {
+			code: ""
+		};
 	},
-	onChange : function (text) {
-		this.setState({text})
+	updateCode: function(newCode) {
+		this.setState({
+			code: newCode,
+		});
+			$(".ReactCodeMirror").on("keypress keyup keydown",function(e){
+				var code = " ";
+				$('#resultat').append(code);
+			});
 	},
-	render : function() {
-		return (
-			<div className="container">
-			<div id="editeur"><Container onChange={this.onChange}/></div>
-			<div className="contain">
-			<label>Your results</label>
-			<div id="resultat"><Message text={this.state.text}/></div>
-			</div>
-			</div>
-			);
+	render: function() {
+		var options = {
+			lineNumbers: true,
+			matchBrackets: true,
+			lineWrapping: true,
+			mode: "markdown"
+		};
+		return <Codemirror value={this.state.code} onChange={this.updateCode} options={options} />
 	}
 });
 
-var Container = React.createClass({
-	getInitialState: function(){
-		return {text:''};
-	},
-	render : function() {
-		return (
-			<div className="editeur">
-			<Input value={this.state.text} onChange={this.props.onChange}/>
-			</div>
-			);
-	}
-});
+ReactDOM.render(<App />, document.getElementById('big'));
 
-ReactDOM.render(
-	<Result />,
-	document.getElementById('big')
-);
 
-var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("code"), {
-	lineNumbers: true,
-	matchBrackets: true,
-	lineWrapping: true,
-	mode: 'markdown'
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Message = React.createClass({
+// 	render : function() {
+// 		return (
+// 			<div>{this.props.text}</div>
+// 			);
+// 	}
+// });
+
+// var Input = React.createClass({
+// 	inputChange: function(e){
+// 		this.props.onChange(e.target.value)
+// 		console.log("inputhcnage")
+// 	},
+// 	componentDidMount: function(){
+// 		console.log("didmount")
+// 		var textArea = document.getElementById('code');
+// 		var myCodeMirror = CodeMirror.fromTextArea(textarea, {
+// 			lineNumbers: true,
+// 			matchBrackets: true,
+// 			lineWrapping: true,
+// 			mode: 'markdown',	
+// 		}); 
+// 			 var result = myCodeMirror.getValue(); 
+
+// 		 // function  prout(){
+// 		 // 	console.log(result);
+// 		 // }
+// 		 // setInterval(prout, 1050)
+
+ 
+// 	},
+// 	handleSubmit : function(e) {
+// 		console.log("fefe")
+// 		e.preventDefault();
+// 		this.setState({text: ' '});
+// 	},
+// 	render : function() {
+// 		return (
+// 			<form onSubmit={this.handleSubmit} >
+// 				<label>Type your Markdown</label>
+// 				<textarea  id="code" value={this.props.text}  cols="100" rows="20"></textarea>
+// 				<button type="submit">Post</button>
+// 			</form>
+// 			);
+// 	}
+// });
+// var Result = React.createClass({
+// 	getInitialState: function(){
+// 		return {text:''};
+// 	},
+// 	onChange : function (text) {
+// 		this.setState({text})
+// 	},
+// 	render : function() {
+// 		return (
+// 			<div className="container">
+// 			<div id="editeur"><Container onChange={this.onChange}/></div>
+// 			<div className="contain">
+// 			<label>Your results</label>
+// 			<div id="resultat"><Message text={this.state.text}/></div>
+// 			</div>
+// 			</div>
+// 			);
+// 	}
+// });
+
+// var Container = React.createClass({
+// 	getInitialState: function(){
+// 		return {text:''};
+// 	},
+// 	render : function() {
+// 		return (
+// 			<Input value={this.state.text} onChange={this.props.onChange}/>
+// 			);
+// 	}
+// });
+
+// ReactDOM.render(
+// 	<Result />,
+// 	document.getElementById('big')
+// );
